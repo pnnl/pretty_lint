2c9d0ec0e868ebdf2ca6586102b1786f36dc1d07
Thom Williams <thom.williams@pnnl.gov>
2018-01-03 09:57:34 -0800

    Linter class placeholders

---
+++ src/Linter/Json/JsonPrettyLinter.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: will202
+ * Date: 1/2/18
+ * Time: 4:09 PM
+ */
+
+namespace Pnnl\PrettyJSONYAML\Linter\Json;
+
+
+class JsonPrettyLinter
+{
+
+}
---
+++ src/Linter/Yaml/YamlPrettyLinter.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: will202
+ * Date: 1/2/18
+ * Time: 4:10 PM
+ */
+
+namespace Pnnl\PrettyJSONYAML\Linter\Yaml;
+
+
+class YamlPrettyLinter
+{
+
+}
