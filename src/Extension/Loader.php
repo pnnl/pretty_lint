61ee35e10d4ceb4b70a9c096d1443ba850e0448f
Thom Williams <thomscode@gmail.com>
2018-03-02 16:04:46 -0800

    Update loader and add filesystem back into linter

--- src/Extension/Loader.php
+++ src/Extension/Loader.php
@@ -10,10 +10,9 @@ namespace Pnnl\PrettyJSONYAML\Extension;
 
 
 use GrumPHP\Extension\ExtensionInterface;
-use Pnnl\PrettyJSONYAML\Task\PrettyJson;
-use Pnnl\PrettyJSONYAML\Task\PrettyJsonFixer;
+use Pnnl\PrettyJSONYAML\Linter\Yaml\YamlPrettyLinter;
+use Pnnl\PrettyJSONYAML\Parser\YamlParser;
 use Pnnl\PrettyJSONYAML\Task\PrettyYaml;
-use Pnnl\PrettyJSONYAML\Task\PrettyYamlFixer;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class Loader implements ExtensionInterface
@@ -26,28 +25,19 @@ class Loader implements ExtensionInterface
      */
     public function load(ContainerBuilder $container)
     {
-        $container->register('task.pretty_json', PrettyJson::class)
-          ->addArgument($container->get('config'))
-          ->addArgument($container->get('process_builder'))
-          ->addArgument($container->get('formatter.prettyjson'))
-          ->addTag('grumphp.task', ['config' => 'pretty_json']);
+        // Create appropriate dependency injection
+        // Create the YAML parser
+        $container->register("parser.yamlparser", YamlParser::class);
 
-        // $container->register('task.pretty_yaml', PrettyYaml::class)
-        //   ->addArgument($container->get('config'))
-        //   ->addArgument($container->get('process_builder'))
-        //   ->addArgument($container->get('formatter.prettyyaml'))
-        //   ->addTag('grumphp.task', ['config' => 'pretty_yaml']);
-        //
-        // $container->register('task.pretty_json_fixer', PrettyJsonFixer::class)
-        //   ->addArgument($container->get('config'))
-        //   ->addArgument($container->get('process_builder'))
-        //   ->addArgument($container->get('formatter.prettyjson'))
-        //   ->addTag('grumphp.task', ['config' => 'pretty_json_fixer']);
-        //
-        // $container->register('task.pretty_yaml_fixer', PrettyYamlFixer::class)
-        //   ->addArgument($container->get('config'))
-        //   ->addArgument($container->get('process_builder'))
-        //   ->addArgument($container->get('formatter.prettyyaml'))
-        //   ->addTag('grumphp.task', ['config' => 'pretty_yaml_fixer']);
+        // Create the YAML Linter
+        $container->register("linter.yamlprettylinter", YamlPrettyLinter::class)
+          ->addArgument($container->get("parser.yamlParser"))
+          ->addArgument($container->get('grumphp.util.filesystem'));
+
+        // Create the YAML task
+        $container->register('task.prettyyaml', PrettyYaml::class)
+          ->addArgument($container->get('config'))
+          ->addArgument($container->get("linter.yamlprettylinter"))
+          ->addTag('grumphp.task', ['config' => 'prettyyaml']);
     }
 }
