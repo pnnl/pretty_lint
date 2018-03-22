428f2618cc0cf34b6a6a5375cfd2879adf42304d
Thom Williams <thomscode@gmail.com>
2018-03-22 07:59:15 -0700

    Add base testing configuration

---
+++ tests/ExceptionTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: will202
+ * Date: 3/22/18
+ * Time: 7:46 AM
+ */
+
+namespace Pnnl\Tests;
+
+class ExceptionTest
+{
+
+}
---
+++ tests/LinterTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: will202
+ * Date: 3/22/18
+ * Time: 7:45 AM
+ */
+
+namespace Pnnl\Tests;
+
+class LinterTest
+{
+
+}
